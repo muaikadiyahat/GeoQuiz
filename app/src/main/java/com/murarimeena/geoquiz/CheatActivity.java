917d2f88db5e44985d82d5c7b7a53cff65cf5bd7
murari meena <murari.me170494@gmail.com>
2017-09-29 02:14:00 +0530

    Oh yes i change cheat activity file just now

--- app/src/main/java/com/murarimeena/geoquiz/CheatActivity.java
+++ app/src/main/java/com/murarimeena/geoquiz/CheatActivity.java
@@ -11,5 +11,7 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+//        this is my first commit on the github by android studio
     }
 }
