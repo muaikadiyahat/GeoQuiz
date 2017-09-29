555c06e50d9ac744458e6faa976376b67bbc4036
murari meena <murari.me170494@gmail.com>
2017-09-29 21:36:27 +0530

    Oh yes i change cheat activity file just now

--- app/src/main/java/com/murarimeena/geoquiz/CheatActivity.java
+++ app/src/main/java/com/murarimeena/geoquiz/CheatActivity.java
@@ -1,9 +1,15 @@
 package com.murarimeena.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
     public static final String EXTRA_ANSWER_IS_TRUE =
             "com.murarimeena.geoquiz.answer_is_true";
 
@@ -12,6 +18,22 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
-//        this is my first commit on the github by android studio version 3
+        mAnswerIsTrue = getIntent().getExtras().getBoolean(EXTRA_ANSWER_IS_TRUE);
+
+        mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
+
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
+
     }
 }
